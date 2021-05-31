fb8c1ddd4487b297f6a854ecd085f24276951f2f
felipem292 <42850866+felipem292@users.noreply.github.com>
2021-05-31 16:39:47 -0500

    Updates

---
+++ precache-manifest.ad46e5ee954c6fc3eb403ada33fce169.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5eb69318fa922c4ea073e33415087b1",
+    "url": "/store_munoz_andres/index.html"
+  },
+  {
+    "revision": "184b3a8fb6cdc365ed4d",
+    "url": "/store_munoz_andres/static/css/2.441070da.chunk.css"
+  },
+  {
+    "revision": "2fcabd7746bcb84f2e28",
+    "url": "/store_munoz_andres/static/css/main.a92de53d.chunk.css"
+  },
+  {
+    "revision": "184b3a8fb6cdc365ed4d",
+    "url": "/store_munoz_andres/static/js/2.6cd4c6a5.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/store_munoz_andres/static/js/2.6cd4c6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fcabd7746bcb84f2e28",
+    "url": "/store_munoz_andres/static/js/main.d8e676ed.chunk.js"
+  },
+  {
+    "revision": "406485a852b9b00804b9",
+    "url": "/store_munoz_andres/static/js/runtime-main.97fc9855.js"
+  },
+  {
+    "revision": "46e33ae44e33077a03180aaa6702d132",
+    "url": "/store_munoz_andres/static/media/error.46e33ae4.jpg"
+  },
+  {
+    "revision": "a343e15346be343af7867fa8f95ad9d5",
+    "url": "/store_munoz_andres/static/media/header-x2.a343e153.png"
+  }
+]);
